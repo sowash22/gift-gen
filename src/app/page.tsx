d48ca318419e6000bebd6929e23b28990a5e6186
Sowndarya Shanmugam <sowndarya.ashok22@gmail.com>
2025-09-01 09:41:18 -0700

    v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  redirect(process.env.NEXT_PUBLIC_PAGE_REDIRECT || '/v1');
+  redirect(process.env.NEXT_PUBLIC_PAGE_REDIRECT || '/v2');
 }
